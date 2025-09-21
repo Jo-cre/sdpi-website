35d19d71fb3ad0c9473613ae85c78f9ae034f925
Jocre <joaopedrosampaio162@gmail.com>
2025-09-21 10:48:44 -0300

    test

--- src/app/api/test-reading/route.ts
+++ src/app/api/test-reading/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 const prisma = new PrismaClient();
 
 // Token fixo para o dispositivo de teste
-const TEST_DEVICE_TOKEN = "aaaa";
+const TEST_DEVICE_TOKEN = "aaaaa";
 
 // Função para gerar valores aleatórios com 2 casas decimais
 function gerarNumeroAleatorio(min: number, max: number) {
